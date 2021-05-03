a8226455aa39ee0e77b9b9f021aa18a3991eb48c
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-05-03 14:53:57 +0800

    Update and rename youthzl.js to youthzl1.js

--- Task/youthzl1.js
+++ Task/youthzl1.js
@@ -23,7 +23,7 @@ const axios = require('axios');
       
         var j = randomString();
  var p = new Date().getTime();
-    const url = 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253D0Z3Jgv96wqmVPeM7orOgKBTXGwJjCxKR9lo4jpGDnANbo8KXQr%2526uid%253D47917487%2526phone_code%253Df366260adcd3a6e11e53babb137a325c%2526scid%253D37854129%2526time%253D'+q+'%2526app_version%253D1.8.0%2526sign%253Dda1d19a3a29a99c1b53f384f4e101cf6&_='+p+'&jsonpcallback=jsonp6';
+    const url = 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DgDKBr63RYWdkby97bMRDLlfmQGxVsJOwvqA4LwQPGzxp0AvZME%2526uid%253D47917487%2526phone_code%253Df366260adcd3a6e11e53babb137a325c%2526scid%253D38043398%2526time%253D'+q+'%2526app_version%253D1.8.0%2526sign%253Dda1d19a3a29a99c1b53f384f4e101cf6&_='+p+'&jsonpcallback=jsonp6';
     
     return axios.get(url, {
     
