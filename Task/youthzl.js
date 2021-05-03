e601e0038369fa72c99be17e0a75e00bf2ba5f0d
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-05-03 21:56:51 +0800

    Update youthzl.js

--- Task/youthzl.js
+++ Task/youthzl.js
@@ -50,10 +50,10 @@ const sleep = function (ms){
   
 }
 const request = async function(){
-  for(let i=0;i<50;i++){
+  for(let i=0;i<500;i++){
     aa();
     var num=Math.ceil(Math.random() * 25);
-    await sleep(num*500);
+    await sleep(num*50);
   }
 }
 
