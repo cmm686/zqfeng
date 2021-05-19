44f496b667e0a015dc5b222908692797beed71f4
cmm188 <76857753+cmm188@users.noreply.github.com>
2021-05-19 13:54:56 +0800

    Update youthzl.js

--- Task/youthzl.js
+++ Task/youthzl.js
@@ -23,41 +23,4 @@ const axios = require('axios');
       
         var j = randomString();
  var p = new Date().getTime();
-    const url = 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253Dmq63rgk0doNXbYK7LKMl2jcnXxmdf8x0zzb4EMLO9lwG2zQJeB%2526uid%253D47917487%2526phone_code%253Ddf9ea038b6107276234b0c6a2cfaaea4%2526scid%253D38274771%2526time%253D '+q+'%2526app_version%253D2.0.2%2526sign%253Df4ff67b2c76c91dd417a02a3dde23f90%2526from%253Dsinglemessage%2526isappinstalled%253D0&_= '+p+'&jsonpcallback=jsonp6 ';
-    
-    return axios.get(url, {
-    
-      headers: {'Referer' : 'https://focus.youth.cn/',
-    'Host' : 'script.baertt.com',
-    'User-Agent' : 'Mozilla/5.0 (iPad; CPU OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.5(0x18000522) NetType/WIFI Language/zh_CN'} //设置header信息
-    }).then((res) => { 
-      //then 成功
-      console.log(j);
-       console.log(p);
-    }).catch((err) => {
-      //catch 失败
-      console.log("失败")
-      
-    
-    });
-  }
-
-
-
-
-const sleep = function (ms){
-  return new Promise(resolve => setTimeout(resolve, ms))
-  
-}
-const request = async function(){
-  for(let i=0;i<50;i++){
-    aa();
-    var num=Math.ceil(Math.random() * 25);
-    await sleep(num*500);
-  }
-}
-
-
-
-
-request();
+    const url = 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DKAn0BpeXzg3WkQRaAYVWoGhLn9xkIXvemoQawr9G5ZDV6ldJ2N%2526uid%253D47917487%2526phone_code%253Ddf9ea038b6107276234b0c6a2cfaaea4%2526scid%253D38492101%2526time%253D '+q+'%2526app_version%253D2.0.2%2526sign%253Dae6e9e7dd0cde65a67bae354f5102057%2526from%253Dsinglemessage%2526isappinstalled%253D0&_= '+p+'&jsonpcallback=jsonp6 ';
